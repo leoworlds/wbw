a67e9abde1e272894bedb806c7297a2daade4b53
LManzhula <leoworlds@gmail.com>
2023-03-29 04:14:26 +0300

    split to Utils

--- src/main/java/leo/main/TextPanel.java
+++ src/main/java/leo/main/TextPanel.java
@@ -5,21 +5,15 @@ import leo.main.setting.theme.Theme;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.font.FontRenderContext;
-import java.awt.geom.AffineTransform;
-import java.util.ArrayList;
 import java.util.List;
 
 public class TextPanel extends JComponent {
 
-//    String text = "I have a public repository on GitHub. I want to replicate/copy it and work on a new project" +
-//            " based on this repository, but I don't want to affect how it is now. " +
-//            "I tried forking it using the GitHub UI but it didn't do anything.";
+    String text1 = "I have a public repository on GitHub. I want to replicate/copy it and work on a new project" +
+            " based on this repository, but I don't want to affect how it is now. " +
+            "I tried forking it using the GitHub UI but it didn't do anything.";
 
     String text = "0000000000 1111111111 2222222222 333 4444444444 5555 666666666666666666666666666666666 777 88888888888888888888888 99999";
-
-    private static final String SPACE = " ";
-    private static final FontRenderContext FONT_RENDER_CONTEXT = new FontRenderContext(new AffineTransform(), true, true);
 
     @Override
     public void paint(Graphics gg) {
@@ -27,25 +21,7 @@ public class TextPanel extends JComponent {
         g.setFont(FontConfig.getFontConfig().getPlainTextFont());
         g.setColor(Theme.getTheme().getTextColor());
 
-        List<String> lines = new ArrayList<>();
-
-        String line = "";
-        String newLine;
-
-        for (String word : text.split(SPACE)) {
-            newLine = line + word + SPACE;
-
-            double newLineWidth = g.getFont().getStringBounds(newLine, FONT_RENDER_CONTEXT).getWidth();
-
-            if (newLineWidth > getWidth()) {
-                lines.add(line);
-                line = word + SPACE;
-            } else {
-                line = newLine;
-            }
-        }
-
-        lines.add(line);
+        List<String> lines = Util.split(text, getWidth());
 
         for (int i = 0; i < lines.size(); i++) {
             g.drawString(lines.get(i), 0, 40 + i*30);
--- src/main/java/leo/main/Util.java
+++ src/main/java/leo/main/Util.java
@@ -1,5 +1,12 @@
 package leo.main;
 
+import leo.main.setting.theme.FontConfig;
+
+import java.awt.font.FontRenderContext;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public final class Util {
@@ -7,6 +14,9 @@ public final class Util {
 
     public static final int WIDTH = 1000;
     public static final int HEIGHT = 700;
+
+    private static final String SPACE = " ";
+    private static final FontRenderContext FONT_RENDER_CONTEXT = new FontRenderContext(new AffineTransform(), true, true);
 
     private static final Random RANDOM = new Random();
 
@@ -16,5 +26,33 @@ public final class Util {
 
     public static String withCapitalLetter(String str) {
         return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
+    }
+
+    public static List<String> split(String text, int width) {
+        List<String> lines = new ArrayList<>();
+
+        String line = "";
+        String newLine;
+
+        for (String word : text.split(SPACE)) {
+            newLine = line + word + SPACE;
+
+            double newLineWidth = getStringBounds(newLine).getWidth();
+
+            if (newLineWidth > width) {
+                lines.add(line);
+                line = word + SPACE;
+            } else {
+                line = newLine;
+            }
+        }
+
+        lines.add(line);
+
+        return lines;
+    }
+
+    public static Rectangle2D getStringBounds(String string) {
+        return FontConfig.getFontConfig().getPlainTextFont().getStringBounds(string, FONT_RENDER_CONTEXT);
     }
 }
